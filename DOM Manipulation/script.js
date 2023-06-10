a010635f3c6abae5d409ccad6beffb30bbeb593c
Raj <rajmkumar369@gmail.com>
2023-06-10 20:53:28 +0530

    Inserting text

--- DOM Manipulation/script.js
+++ DOM Manipulation/script.js
@@ -1,12 +1,24 @@
-function insertElement(){
+// Day 1;
+// Insert Element, Text through Javascript
+//--------------------------------
+// function insertElement(){
     
-    const filter = document.querySelector(".filter");
+//     const filter = document.querySelector(".filter");
 
-    const h1 = document.createElement("h1");
-    h1.textContent = "Insert Adjacent element";
+//     const h1 = document.createElement("h1");
+//     h1.textContent = "Insert Adjacent element";
 
-    filter.insertAdjacentElement("afterbegin", h1);
+//     filter.insertAdjacentElement("afterbegin", h1);
 
+// }
+
+// insertElement();
+
+// ---------------------------------
+
+function insertText(){
+    const item = document.querySelector("li:first-child");
+    item.insertAdjacentText("beforebegin", "insertText");  
 }
 
-insertElement();
+insertText();
