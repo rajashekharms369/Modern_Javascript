ed311bf4f19e6dc857c970721dcb3e202473e07f
Raj <rajmkumar369@gmail.com>
2023-06-21 08:46:08 +0530

    setTimeOut demo

--- Asynchronous_Javascript/script.js
+++ Asynchronous_Javascript/script.js
@@ -1,1 +1,7 @@
-console.log("Hello world");
+setTimeout(changeText,2000);
+
+function changeText(){
+    document.querySelector("h1").textContent = "Hello from callback";
+}
+
+console.log("This is a global scope");
