519b42bd9a70f6702dc49f1d6746185a2fa10910
Raj <rajmkumar369@gmail.com>
2023-06-27 23:09:40 +0530

    AsyncJS update

--- Asynchronous_Javascript/script.js
+++ Asynchronous_Javascript/script.js
@@ -1,7 +1,15 @@
 setTimeout(changeText,2000);
 
 function changeText(){
-    document.querySelector("h1").textContent = "Hello from callback";
+    document.querySelector("#cancel").textContent = "Hello from callback";
 }
+
+const timerId = setTimeout(changeText, 3000);
+
+document.querySelector("#cancel").addEventListener("click", ()=>{
+    console.log(timerId);
+    clearTimeout(timerId);
+    console.log("Timer Cancelled");
+})
 
 console.log("This is a global scope");
