db05d8aa07f580d6e3c44fec1f547d416d9de9c6
Raj <rajmkumar369@gmail.com>
2023-06-28 22:52:59 +0530

    setTimeout function

--- Asynchronous_Javascript/script.js
+++ Asynchronous_Javascript/script.js
@@ -1,15 +1,7 @@
-setTimeout(changeText,2000);
-
 function changeText(){
-    document.querySelector("#cancel").textContent = "Hello from callback";
+    document.querySelector("h1").textContent = "hello form callback";
 }
 
-const timerId = setTimeout(changeText, 3000);
+const timerId = setTimeout(changeText, 4000);
 
-document.querySelector("#cancel").addEventListener("click", ()=>{
-    console.log(timerId);
-    clearTimeout(timerId);
-    console.log("Timer Cancelled");
-})
-
-console.log("This is a global scope");
+timerId();
