571a5d0d11e03bc9c4a7773d345ba73ad20296b1
Raj <rajmkumar369@gmail.com>
2023-07-01 08:53:54 +0530

    call back fucntion update

--- Asynchronous_Javascript/callbacks/script.js
+++ Asynchronous_Javascript/callbacks/script.js
@@ -8,3 +8,15 @@ const post = [
     {title: 'post-one', body: 'This is post one'},
     {title: 'Post-two', body: 'This is post two'}
 ];
+
+function getPosts(){
+    setTimeout(()=>{
+        post.forEach(function(post){
+            const div = document.createElement('div');
+            div.innerHTML = `<strong>${post.title}</strong>-${post.body}`;
+            document.querySelector('#post').appendChild(div);
+        });
+    },1000);
+}
+
+getPosts();
