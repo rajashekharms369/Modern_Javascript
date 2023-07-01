f0e9942683cb75eafac6e4f431a76494dd33acde
Raj <rajmkumar369@gmail.com>
2023-07-01 08:46:59 +0530

    callback function update

--- Asynchronous_Javascript/callbacks/script.js
+++ Asynchronous_Javascript/callbacks/script.js
@@ -1,5 +1,13 @@
-document.querySelector("button").addEventListener("click", toggle);
+// document.querySelector("button").addEventListener("click", toggle);
 
-function toggle(e){
-    e.target.classList.toggle("danger");
-}
+// function toggle(e){
+//     e.target.classList.toggle("danger");
+// }
+
+// const post = [
+//     {title: 'post-one', body: 'This is post one'},
+//     {title: 'Post-two', body: 'This is post two'}
+// ];
+
+var obj = {name:'Raj', age:25};
+console.log(obj);
