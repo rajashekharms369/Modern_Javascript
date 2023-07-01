99aab15a1de512e49c10cac40114a1e16ca5ea60
Raj <rajmkumar369@gmail.com>
2023-07-01 08:47:53 +0530

    js callback update

--- Asynchronous_Javascript/callbacks/script.js
+++ Asynchronous_Javascript/callbacks/script.js
@@ -4,10 +4,7 @@
 //     e.target.classList.toggle("danger");
 // }
 
-// const post = [
-//     {title: 'post-one', body: 'This is post one'},
-//     {title: 'Post-two', body: 'This is post two'}
-// ];
-
-var obj = {name:'Raj', age:25};
-console.log(obj);
+const post = [
+    {title: 'post-one', body: 'This is post one'},
+    {title: 'Post-two', body: 'This is post two'}
+];
