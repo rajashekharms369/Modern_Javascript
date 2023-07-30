6b7165d65c6f201e5e4b96ef84d3b97ca0b8cfd6
Raj <rajmkumar369@gmail.com>
2023-07-30 09:42:07 +0530

    async js update

--- Asynchronous_Javascript/Callback_Hell/script.js
+++ Asynchronous_Javascript/Callback_Hell/script.js
@@ -1,25 +1,40 @@
-const promise = new Promise((resolve, reject)=>{
-    setTimeout(() => {
-        let error = false;
-        if(!error){
-            resolve({name:"John", age:33})
-        }else{
-            reject("Error: Something went wrong");
-        }
-    }, 1000);
-});
+// const promise = new Promise((resolve, reject)=>{
+//     setTimeout(() => {
+//         let error = false;
+//         if(!error){
+//             resolve({name:"John", age:33})
+//         }else{
+//             reject("Error: Something went wrong");
+//         }
+//     }, 1000);
+// });
 
-promise
-    .then((user)=>{
-        console.log(user);
-    })
-    .then((name)=>{
-        console.log(name);
-        return name.length;
-    })
-    .then((nameLength)=>{
-        console.log(nameLength);
-    })
-    .catch((error)=>console.log(error))
-    .then(()=>console.log("This will run no matter what"));
-console.log("Hellow world");
+// promise
+//     .then((user)=>{
+//         console.log(user);
+//     })
+//     .then((name)=>{
+//         console.log(name);
+//         return name.length;
+//     })
+//     .then((nameLength)=>{
+//         console.log(nameLength);
+//     })
+//     .catch((error)=>console.log(error))
+//     .then(()=>console.log("This will run no matter what"));
+
+function resolveAfter2Seconds(){
+    return new Promise(resolve=>{
+        setTimeout(()=>{
+            resolve("resolved");
+        },2000);
+    });
+}
+
+async function asyncCall(){
+    console.log("calling");
+    const result = await resolveAfter2Seconds();
+    console.log(result);
+}
+
+asyncCall();
