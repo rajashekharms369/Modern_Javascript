b18a4dc7531ab704acc1053268f42936c44b574f
Raj <rajmkumar369@gmail.com>
2023-08-07 08:31:34 +0530

    js fetchapi update

--- Asynchronous_Javascript/typicode-todos/script.js
+++ Asynchronous_Javascript/typicode-todos/script.js
@@ -24,7 +24,10 @@ const getTodos = () => {
 
 const addToDoDom = (todo) => {
     const div = document.createElement("div");
+    
     div.appendChild(document.createTextNode(todo.title));
+
+    div.classList.add('todo');
 
     div.setAttribute("data-id", todo.id);
 
@@ -35,21 +38,35 @@ const addToDoDom = (todo) => {
     document.getElementById("todo-list").appendChild(div);
 };
 
+const toggleCompleted = (e) => {
+    if(e.target.classList.contains("todo")){
+        e.target.classList.toggle("done");
+    }
+};
+
 const init = () => {
     document.addEventListener("DOMContentLoaded", getTodos);
     document.querySelector("#todo-form").addEventListener("submit",createTodo);
+    document.querySelector("#todo-list").addEventListener("click", toggleCompleted);
 }
 
-    const createTodo = (e) => {
-        try{
-            e.preventDefault();
-            console.log("1");
-        }
-        catch{
-            console.log("Some shit went wrong");
-        }
-        e.preventDefault();
+const createTodo = (e) => {
+    e.preventDefault();
+    const newTodo = {
+        title: e.target.firstElementChild.value,
+        completed: false
     }
+
+    fetch(apiUrl, {
+        method: "POST",
+        body: JSON.stringify(newTodo),
+        headers:{
+            "Content-type":"application/json"
+        }
+    })
+    .then(res => res.json())
+    .then(data => addToDoDom(data))
+};
 
 // getTodos();
 init();
