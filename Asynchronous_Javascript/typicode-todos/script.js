9a25043e499738a8d9729edb18a5f59f17daac1f
Raj <rajmkumar369@gmail.com>
2023-08-07 07:28:41 +0530

    simple todo app

--- Asynchronous_Javascript/typicode-todos/script.js
+++ Asynchronous_Javascript/typicode-todos/script.js
@@ -1,13 +1,40 @@
+// const apiUrl = "https://jsonplaceholder.typicode.com/todos";
+
+// const getTodos = () => {
+//     fetch(apiUrl + "?_limit=5")
+//     .then((res) => res.json())
+//     .then((data) => {
+//         console.log(data);
+//     });
+// };
+
+// getTodos();
+
+// console.log("Hello world");
+
 const apiUrl = "https://jsonplaceholder.typicode.com/todos";
 
 const getTodos = () => {
-    fetch(apiUrl + "?_limit=5")
-    .then((res) => res.json())
-    .then((data) => {
-        console.log(data);
+    fetch(apiUrl+"?_limit=5")
+    .then((res)=>res.json())
+    .then((data)=>{
+        data.forEach((todo) => addToDoDom(todo));
     });
 };
 
+const addToDoDom = (todo) => {
+    const div = document.createElement("div");
+    div.appendChild(document.createTextNode(todo.title));
+
+    div.setAttribute("data-id", todo.id);
+
+    if(todo.completed){
+        div.classList.add("done");
+    }
+
+    document.getElementById("todo-list").appendChild(div);
+}
+
 getTodos();
 
-console.log("Hello world");
+console.log("hello world");
