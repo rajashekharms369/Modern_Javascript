1c7ac50e6ac829d3d38e6a63579d8c863b960485
Raj <rajmkumar369@gmail.com>
2023-08-10 08:09:27 +0530

    js async await

--- Asynchronous_Javascript/typicode-todos/fetchapi_async_await/Error_Handling_fetchapi/script.js
+++ Asynchronous_Javascript/typicode-todos/fetchapi_async_await/Error_Handling_fetchapi/script.js
@@ -20,3 +20,27 @@
 //     .catch((error) =>{
 //         console.log(error)
 //     });
+
+// const promise = new Promise((resolve, reject)=>{
+//     setTimeout(()=>{
+//         resolve({name:"John", age:20});
+//     },1000);
+// });
+
+
+// async function getPromise(){
+//     const response = await promise;
+//     console.log(response);
+// }
+
+// getPromise();
+
+
+async function getUsers(){
+    const res = await fetch("https://jsonplaceholder.typicode.com/users");
+
+    const data = await res.json();
+    console.log(data);
+}
+
+getUsers();
