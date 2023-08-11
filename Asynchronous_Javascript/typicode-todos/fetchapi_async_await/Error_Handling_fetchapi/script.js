56006e25355cfb5a86d815d132ad8c55a98d46f7
Raj <rajmkumar369@gmail.com>
2023-08-11 07:17:03 +0530

    js update

--- Asynchronous_Javascript/typicode-todos/fetchapi_async_await/Error_Handling_fetchapi/script.js
+++ Asynchronous_Javascript/typicode-todos/fetchapi_async_await/Error_Handling_fetchapi/script.js
@@ -36,11 +36,40 @@
 // getPromise();
 
 
-async function getUsers(){
-    const res = await fetch("https://jsonplaceholder.typicode.com/users");
+// async function getUsers(){
+//     const res = await fetch("https://jsonplaceholder.typicode.com/users");
 
-    const data = await res.json();
-    console.log(data);
-}
+//     const data = await res.json();
+//     console.log(data);
+// }
+
+// getUsers();
+
+// try{
+//     console.log(x);
+// } catch(error){
+//     console.log(error);
+// }
+
+
+const getUsers = async () =>{
+    
+    try{
+        // const response = await fetch("https://jsonplaceholder.typicode.com/users");
+        // const data = await response.json();
+        const response =  await fetch("https://httpstat.us/500");
+
+        if(!response.ok){
+            throw new Error("Request failed");
+        }
+
+        const data = await response.text();
+        
+        console.log(data);
+    }
+    catch(error){
+        console.log(error);
+    }
+};
 
 getUsers();
