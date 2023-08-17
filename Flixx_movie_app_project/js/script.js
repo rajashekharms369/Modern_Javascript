07ec45456086bbd4c6fb98dbe95fdea1e73fcdb2
Raj <rajmkumar369@gmail.com>
2023-08-17 07:34:36 +0530

    flixx prject update

--- Flixx_movie_app_project/js/script.js
+++ Flixx_movie_app_project/js/script.js
@@ -1,2 +1,27 @@
-console.log("hello world");
-console.log("Hello world");
+const global = {
+    currentPage: window.location.pathname,
+};
+
+function init(){
+    switch(global.currentPage){
+        case "/":
+        case "/index.html":
+            console.log('Home');
+            break;
+        case "shows.html":
+            console.log("shows");
+            break;
+        case "/movie-details.html":
+            console.log("Movie Details");
+            break;
+        case "/tv-details.html":
+            console.log("TV details");
+            break;
+        case "/search.html":
+            console.log("Search");
+            break;
+    }
+}
+
+document.addEventListener("DOMContentLoaded", init);
+ 
