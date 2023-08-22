f9af21a24a371ab00faa7cd17a9d7cba1f391751
Raj <rajmkumar369@gmail.com>
2023-08-22 07:25:32 +0530

    js geolocation methods

--- Web_browser_API/GeoLocation_api/script.js
+++ Web_browser_API/GeoLocation_api/script.js
@@ -1,1 +1,19 @@
-console.log("Hello world");
+function curSuccess(pos){
+    const coords = pos.coords;
+    console.log(`Latitude: ${coords.latitude}`);
+    console.log(`Longitude: ${coords.longitude}`);
+    console.log(`Within: ${coords.accuracy}+ meters`)
+    console.log(pos);
+}
+
+function curError(err){
+    console.log(`Error: ${err.code} - ${err.message}`);
+}
+
+const curOptions = {
+    enableHighAccuracy: true,
+    timeout: 5000,
+    maximumAge: 0
+};
+
+navigator.geolocation.getCurrentPosition(curSuccess, curError, curOptions);
