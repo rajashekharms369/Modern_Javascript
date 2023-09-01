35b1de1eddc00d1c365779afddbd243cb00b096c
Raj <rajmkumar369@gmail.com>
2023-09-01 23:37:44 +0530

    js app update

---
+++ Web_browser_API/Music_player_project/script.js
@@ -0,0 +1,36 @@
+const musicContainer = document.getElementById("music-container");
+const playBtn = document.getElementById("play");
+const prevBtn = document.getElementById("prev");
+const nextBtn = document.getElementById("next");
+const audio = document.getElementById("audio");
+const progress = document.getElementById("progress");
+const progressContainer = document.getElementById("progress-container");
+const title = document.getElementById("title");
+const cover = document.getElementById("cover");
+
+
+// Song title
+const songs = ["hey", "summer", "ukelele"];
+
+// Keep track of song
+let songIndex = 2;
+
+// Initially load song deatils
+loadSong(songs[songIndex]);
+
+function loadSong(song){
+    title.innerText = song;
+    audio.src = `music/${song}.mp3`;
+    audio.src = `album_images/${song}.jpg`;
+}
+
+playBtn.addEventListener('click', () => {
+    const isPlaying = musicContainer.classList.contains("play");
+    if(isPlaying){
+        pauseSong();
+    }else{
+        playSong();
+    }
+})
+
+console.log("hello world");
