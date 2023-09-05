03a1716cba80347fa4b2147b4a5dbe42c09cfb94
Raj <rajmkumar369@gmail.com>
2023-09-05 20:30:24 +0530

    js speech recognition project

--- Web_browser_API/speech_Synthesis_api/script.js
+++ Web_browser_API/speech_Synthesis_api/script.js
@@ -1,18 +1,46 @@
+const voiceSelect = document.getElementById('voice-select');
+
 const synth = window.speechSynthesis;
+let voices;
 
-function onSubmit(e){
-    e.preventDefault();
-    const textInput = document.getElementById("text-input");
+function addVoicesToSelect() {
+  voices = synth.getVoices();
 
-    const utterThis = new SpeechSynthesisUtterance(textInput.value);
+  for (let i = 0; i < voices.length; i++) {
+    const option = document.createElement('option');
+    option.textContent = `${voices[i].name} - ${voices[i].lang}`;
 
-    synth.speak(utterThis);
+    if (voices[i].default) {
+      option.textContent += ' - DEFAULT';
+    }
+
+    option.setAttribute('data-lang', voices[i].lang);
+    option.setAttribute('data-name', voices[i].name);
+    voiceSelect.appendChild(option);
+  }
 }
 
-const voiceSelect = document.getElementById("voice-select");
+function onSubmit(e) {
+  e.preventDefault();
 
-function addVoicesToSelect(){
+  const textInput = document.getElementById('text-input');
 
+  const utterThis = new SpeechSynthesisUtterance(textInput.value);
+
+  const selectedOption =
+    voiceSelect.selectedOptions[0].getAttribute('data-name');
+  for (let i = 0; i < voices.length; i++) {
+    if (voices[i].name === selectedOption) {
+      utterThis.voice = voices[i];
+    }
+  }
+
+  synth.speak(utterThis);
 }
 
-document.getElementById("form").addEventListener("submit", onSubmit);
+addVoicesToSelect();
+if (speechSynthesis.onvoiceschanged !== undefined) {
+  speechSynthesis.onvoiceschanged = addVoicesToSelect;
+}
+
+document.getElementById('form').addEventListener('submit', onSubmit);
