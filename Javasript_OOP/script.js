6d7e20b017156506d871a5c26a670f0e650403c8
Raj <rajmkumar369@gmail.com>
2023-09-09 06:29:37 +0530

    js oop update

--- Javasript_OOP/script.js
+++ Javasript_OOP/script.js
@@ -1,8 +1,22 @@
-    const rectangle = {
-        name: "Rectangle 1",
-        width: 10,
-        height: 10
-    };
+class Shape{
+    constructor(name){
+        this.name = name;
+    }
 
-    console.log(rectangle.name);
-    console.log("Hello world");
+    logName(){
+        console.log(this.name);
+    }
+}
+
+class Rectangle extends Shape{
+    constructor(name, width, height){
+        super(name);
+        this.width = width;
+        this.height = height;
+    }
+}
+
+const rect = new Rectangle("Rect 1", 20, 20);
+console.log("Hello world");
+rect.logName();
+console.log("Good morning");
