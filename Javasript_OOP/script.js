afe72a6468eb685bab58d0de4ec39ac665defd83
Raj <rajmkumar369@gmail.com>
2023-09-10 08:45:52 +0530

    js oop update

--- Javasript_OOP/script.js
+++ Javasript_OOP/script.js
@@ -65,10 +65,60 @@ function Person(firstName, lastName){
 
 // console.log(person1.firstName+" "+person1.lastName);
 
-function Rectangle(name){
-    this.name = name;
-}
+// function Rectangle(name){
+//     this.name = name;
+// }
 
-const rect1 = new Rectangle("Rectangle 1");
+// const rect1 = new Rectangle("Rectangle 1");
 
-console.log(rect1);
+// console.log(rect1);
+
+
+// function Rectangle(name, width, height){
+//     this.name = name;
+//     this.width = width;
+//     this.height = height;
+//     this.area = function(){
+//         return this.width*this.height;
+//     }
+// }
+
+// const rect1 = new Rectangle("Rect 1", 10, 10);
+
+// const rect2 = new Rectangle("Rect 2", 20, 10);
+
+// // console.log(rect1["width"]);
+
+// rect1.perimeter = () => {
+//     return 2*(rect1.width + rect1.height);  
+// }
+// // console.log(rect1);
+// console.log(rect1.perimeter());
+
+// delete rect1.perimeter;
+
+// console.log(rect1);
+
+
+// // Checking for the property
+// console.log(rect1.hasOwnProperty("width"));
+// console.log(rect1.hasOwnProperty("color"));
+
+// // Getting all the keys
+// console.log(Object.keys(rect1));
+
+// // Getting all the values of the object
+// console.log(Object.values(rect1));
+
+// // Getting all the entries
+// console.log(Object.entries(rect1));
+
+// for(let [key, value] of Object.entries(rect1)){
+//     if(typeof value != "function"){
+//         console.log(`${key} - ${value}`);
+//     }
+// }
+
+
+
+// Prototype
