5f85f9ef451cfb4ab829023efa3107f3e0b11894
Raj <rajmkumar369@gmail.com>
2023-09-10 08:25:35 +0530

    js oop udpate

--- Javasript_OOP/script.js
+++ Javasript_OOP/script.js
@@ -14,3 +14,61 @@
 // const app = new App();
 
 
+// class Person{
+//     constructor(firstName, lastName){
+//         this._firstName = firstName;
+//         this._lastName = lastName;
+//     }
+
+//     get firstName(){
+//         console.log(this._firstName);
+//     }
+
+//     get lastName(){
+//         console.log(this._lastName);
+//     }
+// }
+
+// const person1 = new Person("John", "Doe");
+// person1.firstName;
+// person1.lastName;
+
+
+function Person(firstName, lastName){
+    this._firstName = firstName;
+    this._lastName = lastName;
+
+    Object.defineProperty(this, "firstName", {
+        get: function(){
+            return this._firstName;
+        },
+        set: function(value){
+            this._firstName = value;
+        },
+    });
+
+    Object.defineProperty(this, "lastName",{
+        get: function(){
+            return this._lastName;
+        },
+        set: function(value){
+            this._lastName = value;
+        },
+    });
+}
+
+
+// Person.prototype.capitalizeFirst = function(value){
+//     return value.charAt(0).toUpperCase()+value.slice(1);
+// }
+// const person1 = new Person("John", "Doe");
+
+// console.log(person1.firstName+" "+person1.lastName);
+
+function Rectangle(name){
+    this.name = name;
+}
+
+const rect1 = new Rectangle("Rectangle 1");
+
+console.log(rect1);
