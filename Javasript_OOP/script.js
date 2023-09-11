efc29fa74566f1f5723cce5ec31430d46686e951
Raj <rajmkumar369@gmail.com>
2023-09-11 06:45:02 +0530

    js oop update

--- Javasript_OOP/script.js
+++ Javasript_OOP/script.js
@@ -122,3 +122,14 @@ function Person(firstName, lastName){
 
 
 // Prototype
+function Rectangle(name, width, height){
+    this.name = name;
+    this.width = width;
+    this.height = height;
+    this.area = function(){
+        return this.width*this.height;
+    }
+}
+
+const rect = new Rectangle("Rect", 10, 10);
+console.log(Object.getPrototypeOf(rect))
