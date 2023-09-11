92502b1111e597678a3e37444f4870eb49a2e83b
Raj <rajmkumar369@gmail.com>
2023-09-11 07:09:40 +0530

    js oop prototype update

--- Javasript_OOP/script.js
+++ Javasript_OOP/script.js
@@ -137,4 +137,34 @@ function Person(firstName, lastName){
 // console.log(rect);
 // console.log(rect.area());
 
+const rectanglePrototypes = {
+    area: function(){
+        return this.width*this.height;
+    },
+    perimeter: function(){
+        return 2*(this.width+this.height);
+    },
+    isSquare: function(){
+        return this.height === this.width;
+    }
+}
 
+
+function createRectangle(height, width){
+    return Object.create(rectanglePrototypes,{
+        height: {
+            value: height
+        },
+        width:{
+            value: width
+        },
+    });
+}
+
+
+const rect = createRectangle(10, 10);
+console.log(rect);
+console.log(rect.area());
+
+const rect2 = createRectangle(10, 20);
+console.log(rect2);
