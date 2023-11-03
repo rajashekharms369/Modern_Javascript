fbc1a853d853cb93077fd1134bbd750dfe80af52
Raj <rajmkumar369@gmail.com>
2023-11-03 22:42:21 +0530

    Bootstrap update

---
+++ web_dev_playground/Bootstrap_playground/script.js
@@ -0,0 +1,13 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const progressBar = document.getElementById("pb");
+
+    let i = 0;
+    const interval = setInterval(function () {
+        progressBar.style.width = i + "%";
+        i+=20;
+
+        if (i > 100) {
+            clearInterval(interval);
+        }
+    }, 1000);
+});
