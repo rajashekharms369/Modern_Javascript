0d90610093f61d1d2b6508a0dade4e69b11522bc
Raj <rajmkumar369@gmail.com>
2024-01-02 10:43:22 +0530

    angularjs update

--- web_dev_playground/playGround001.js
+++ web_dev_playground/playGround001.js
@@ -1,7 +1,1 @@
-function validateForm(){
-    let x = document.forms["myform"]["fname"].value;
-    if(x==""){
-        alert("The input field cannot be empty");
-        return false;
-    }
-}
+git 
